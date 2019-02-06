0ded62cfe72db35dc6f56b42a6c12f06e06cac34
unknown <nap3831@gmail.com>
2019-02-06 23:00:23 +0700

    add banner on home page

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,69 +1,81 @@
 import React , {Component} from 'react';
 import '../Assets/css/styles.css';
-import {Collapse, Navbar, NavbarToggler, Nav, NavItem  } from 'reactstrap';
- import {NavLink , Link} from 'react-router-dom';
- import Apple from '../Assets/images/Apple.png';
+import {Link , NavLink} from 'react-router-dom';
+import {Navbar,
+        NavbarToggler,
+        Collapse,
+        Nav,
+        NavItem,
+        } from 'reactstrap';
+import Apple from '../Assets/images/Apple.png';
 
  class Navigation extends Component{
  constructor(props){
    super(props);
-   this.closeNav = this.closeNav.bind(this);
-   this.toggle = this.toggle.bind(this);
+  this.closeNav = this.closeNav.bind(this);
+  
+   this.toggleNavigation = this.toggleNavigation.bind(this);
    this.state = {
     isOpen : false
    };
   }
+  
+  toggleNavigation(){
+    this.setState({
+    isOpen : !this.state.isOpen
+  })
+  }
   closeNav(){
     this.setState({
-isOpen : false
-    });
+    isOpen:false
+    })
   }
-   toggle() {
-     this.setState({
-     isOpen: !this.state.isOpen
-    });
-   
- }
-
-  render(){
-  
-
  
-      return(
-        <div>
-        <Navbar  className="text-center  fixed-top " color="dark" dark white  expand="md" >
-      
-        <Link className="navbar-brand" to="/">
-          <img className="setAppleIcon" src={Apple}  alt="description image"/>
-          </Link>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-         
-            <Nav className="mx-auto" navbar>
-              <NavItem  className="navbarLink ">
-                <NavLink to="/" onClick={this.closeNav}>Home</NavLink>
-              </NavItem>
-              <NavItem  className="navbarLink ">
-                <NavLink to="/" onClick={this.closeNav}>Home</NavLink>
-              </NavItem>
-              <NavItem  className="navbarLink ">
-                <NavLink to="/" onClick={this.closeNav}>Home</NavLink>
-              </NavItem>
-              <NavItem  className="navbarLink ">
-                <NavLink to="/" onClick={this.closeNav}>Home</NavLink>
-              </NavItem>
-              <NavItem  className="navbarLink ">
-                <NavLink to="/" onClick={this.closeNav}>Home</NavLink>
-              </NavItem>
+  render(){
+  return(
 
-             
+    <div>
+      <Navbar className="fixed-top" dark expand="md" style={{background:'rgba(0,0,0,0.8)',
+      padding:'6px'
+}}>
+  < Link to="/">
+  <img className="img-alignment" src={Apple} alt="Logo" width="28px" 
+  style={{marginLeft: '254px',
+}}/>
+  </Link>
+  <NavbarToggler onClick={this.toggleNavigation} className="mr-2" />
 
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    
+  <Collapse  isOpen={this.state.isOpen} navbar>
+    <Nav   navbar  >
+      <NavItem>
+        <NavLink className="navbarLink "  to="/" onClick={this.closeNav}>Mac</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="navbarLink " to="/" onClick={this.closeNav}>iPad</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="navbarLink " to="/" onClick={this.closeNav}>iPhone</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="navbarLink " to="/" onClick={this.closeNav}>Watch</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="navbarLink " to="/" onClick={this.closeNav}>TV</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="navbarLink " to="/" onClick={this.closeNav}>Music</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="navbarLink " to="/" onClick={this.closeNav}>Support</NavLink>
+      </NavItem>
+    </Nav>
+  </Collapse>
+</Navbar>
+    </div>
       );
-}
+ }
     }
 export default Navigation;
+
+
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
-
+import banner from '../Assets/images/Banner.jpg';
 export default class Home extends Component {
   render() {
     return (
-      <div className="topMargin">
-          Home Pages
-      </div>
+      <div >
+      <img className=" banner" alt="BANNER"  src={banner}/>
+
+<p className="text-white " style={{
+    marginTop:'-25%',
+    fontSize: '40px',
+    lineHeight: 1.04167,
+    fontWeight: 500,
+    textAlign:"center",
+    letterSpacing: '0.006em'}}>Shop for your favorite Apple&nbsp;products.</p>
+
+</div >
     )
   }
 }
